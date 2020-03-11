3613c5dc7302896172eda7f9d5ea0639034db056
handsome-fish <gitfish@163.com>
2020-03-11 18:09:05 +0800

    字符串合并

--- string_join.py
+++ string_join.py
@@ -1,3 +1,19 @@
 # 字符串合并
+#
+# 将列表中的字符串合并为字符串
+# 可以与字符串分割结合，来去除字符串中不想留下的项
+
+
+list = ['i', 'love', 'python']
+print(' '.join(list))
+
+
+# 去除字符串中不需要的字符
 import re
-re.split()
+str = '            i^%*&love    ^*&python  '
+print(' '.join(re.split(r'\W+', str.strip())))
+
+
+
+
+
