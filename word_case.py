1003adc784ccc82b5ddf1fb5a206f64a1ac42415
handsome-fish <gitfish@163.com>
2020-03-11 16:54:19 +0800

    单词大小写

---
+++ word_case.py
@@ -0,0 +1,10 @@
+# 单词大小写
+# 面对一个字符串，想将里面的单词首字母大写，只需要调用title()函数
+# 而所有的字母大写只需要调用upper()函数
+# 字符串首字母大写则是调用capitalize()函数即可
+
+
+str = 'i love python.'
+print(str.title())
+print(str.upper())
+print(str.capitalize())
